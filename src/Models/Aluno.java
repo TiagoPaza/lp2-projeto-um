23b0352b83f142dcc5651eb323ed0444f4baf406
Tiago Paza <tiago.paza9@gmail.com>
2020-04-27 19:31:51 -0300

    :sparkles: Implementando terceira parte (cinco)

---
+++ src/Models/Aluno.java
@@ -0,0 +1,40 @@
+package Models;
+
+public class Aluno {
+    private String nomeCompleto;
+    private String cpf;
+    private int codUsuario;
+    private int matricula;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getCodUsuario() {
+        return codUsuario;
+    }
+
+    public void setCodUsuario(int codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
---
+++ src/Models/AlunoPosGraduacao.java
@@ -0,0 +1,49 @@
+package Models;
+
+public class AlunoPosGraduacao extends Aluno {
+    private String graduacao;
+    private String instituicao;
+    private String dataInicio;
+    private String dataConclusao;
+    private Curso curso;
+
+    public String getGraduacao() {
+        return graduacao;
+    }
+
+    public void setGraduacao(String graduacao) {
+        this.graduacao = graduacao;
+    }
+
+    public String getInstituicao() {
+        return instituicao;
+    }
+
+    public void setInstituicao(String instituicao) {
+        this.instituicao = instituicao;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public String getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(String dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
